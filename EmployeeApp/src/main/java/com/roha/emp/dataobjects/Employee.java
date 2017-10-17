eb1ebe60f930ef7b0d54a652d8aed5b2b9bea19f
rohandm <rohandm@users.noreply.github.com>
2017-10-17 18:42:39 -0500

    Add files via upload

---
+++ EmployeeApp/src/main/java/com/roha/emp/dataobjects/Employee.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.roha.emp.dataobjects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private long id;
+
+	private String firstName;
+        private String middleInitial;
+	private String lastName;
+        private String dateOfBirth;
+        private String dateOfEmployment;
+        private String status;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the middleInitial
+     */
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    /**
+     * @param middleInitial the middleInitial to set
+     */
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the dateOfEmployment
+     */
+    public String getDateOfEmployment() {
+        return dateOfEmployment;
+    }
+
+    /**
+     * @param dateOfEmployment the dateOfEmployment to set
+     */
+    public void setDateOfEmployment(String dateOfEmployment) {
+        this.dateOfEmployment = dateOfEmployment;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
